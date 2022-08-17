b598ec4a6a8b62e6a52692441af0ff86b5795dc0
jhonathan silva <jhonathan.silva@interne.com.br>
2022-08-17 16:30:46 -0300

    ClassConceito

---
+++ ClasseConceito/src/Principal.java
@@ -0,0 +1,14 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Pessoa pes = new Pessoa();
+		pes.nomePessoa = "JOÃO DO NASCIMENTO";
+		pes.idadePessoa = 15;
+		pes.mostraDados(pes.nomePessoa, pes.idadePessoa);
+
+	}
+
+}
